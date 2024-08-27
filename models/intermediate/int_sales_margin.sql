31057236fbb73a6b0d0b5de3a6059e9245e1f089
c.gastello.r@gmail.com <c.gastello.r@gmail.com>
2024-08-27 21:16:29 +0000

    Agregando el primer intermedio margin y purchase_cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,19 @@
+WITH source AS (
+    SELECT *
+    FROM {{ref("stg_raw__sales")}}
+    LEFT JOIN {{ref("stg_raw__product")}}
+    ON stg_raw__product.products_id = stg_raw__sales.product_id
+),
+    renamed AS(
+        SELECT *
+        FROM source
+    )
+
+    SELECT
+        product_id,
+        revenue,
+        CAST(purchase_price AS NUMERIC) AS purchase_price,
+        quantity,
+        (CAST(purchase_price AS NUMERIC)) * quantity AS purchase_cost,
+        revenue - (CAST(purchase_price AS NUMERIC)) * quantity AS margin
+    FROM renamed
